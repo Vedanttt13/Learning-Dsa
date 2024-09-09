652feda1a626326011074e2059e71be313b2e6f4
Vedant Chaudhari <117809601+Vedanttt13@users.noreply.github.com>
2024-09-09 14:50:26 +0530

    Update BubbleSortOptimized.java

--- Sorting/BubbleSortOptimized.java
+++ Sorting/BubbleSortOptimized.java
@@ -2,10 +2,9 @@ public class BubbleSortOptimized {
     public static void main(String[] args) {
         int[] a = {3, 4, 5, 2, 54, 7, 9};
         int n = a.length;
-        boolean flag;
 
         for (int i = 0; i < n; i++) {
-            flag = true;  // Set flag to true at the start of each iteration
+             boolean flag = true;  // Set flag to true at the start of each iteration
 
             for (int j = 0; j < n - i - 1; j++) {
                 if (a[j] > a[j + 1]) {
